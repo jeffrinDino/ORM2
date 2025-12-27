fe5d7edf87058316fee497eb6a0f99c5ac4d5341
Dino <dinorijo96@gmail.com>
2025-12-27 10:33:25 +0530

    success

---
+++ car/carapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib import admin
+class Car(models.Model):
+    brand_name=models.CharField()
+    car_name=models.CharField()
+    enginenum=models.IntegerField()
+    release_date=models.DateField()
+
+class CarAdmin(admin.ModelAdmin):
+    list_display=('brand_name', 'car_name', 'enginenum', 'release_date')
